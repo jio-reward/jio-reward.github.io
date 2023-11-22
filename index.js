6b5c95c50c9dcd35a18b0cc8859167e68c86b3c0
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-11-22 19:40:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-index file
+console.log("started script");
+let isLoggged = document.cookie.isLogged ? document.cookie.isLogged : false;
+let isSpin = document.cookie.isSpin ? document.cookie.isSpin : false;
+let winPrize document.cookie.winningPrize ? document.cookie.winningPrize : "💩";
