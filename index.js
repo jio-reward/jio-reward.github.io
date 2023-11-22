e182e9cbeb6b5c6734e6435d7b5e38097cf6fc86
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-11-22 23:22:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ console.log("started script");
 let isLoggged = document.cookie.isLogged ? document.cookie.isLogged : false;
 let isSpin = document.cookie.isSpin ? document.cookie.isSpin : false;
 let winPrize document.cookie.winningPrize ? document.cookie.winningPrize : "💩";
+
+function addNumberToQueue(num){
+  fetch(`https://kvdb.io/7W8WYYhyFEsSmQPWVri4Zs/kvs_/${num}`, {
+method: "POST",
+body: num
+});
+  return true;
+}
+
+function checkNumIfIsInQueue(num){
+
+}
