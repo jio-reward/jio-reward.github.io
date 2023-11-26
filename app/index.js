ea7b75e14eb8afb8fb8864a49cd6ddd01f82dc84
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-11-26 19:22:20 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -11,5 +11,19 @@
 <body>
   hii
   <!-- Your app content -->
+  <script>
+  // script.js
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/app/sw.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+
+  </script>
 </body>
 </html>
