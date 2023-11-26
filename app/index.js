025fbd56f4db5889688cd2e706547d1da97a44e2
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-11-26 19:18:36 +0530

    Create index.js

---
+++ app/index.js
@@ -0,0 +1,15 @@
+<!-- index.html -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="manifest" href="app/manifest.json">
+  <title>My PWA</title>
+</head>
+<body>
+  hii
+  <!-- Your app content -->
+</body>
+</html>
